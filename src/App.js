418c4461589f33990e898d9e12a19348979e6fe6
Ali Khorsandfard <secure73@gmail.com>
2025-09-15 11:32:54 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
 function App() {
   return (
-    <div className="App">
-      <div className="bg-blue-500">
-          hello world from app.js
-      </div>
-    </div>
+    /**Router wrap all app! it means it is available for whole of application. please let index simple! do not add route to index, please!!! */
+    /**Router component is similar to Router hardware in TCP/IP network , having one dosent mean it work , you shall configure it */
+    /**equivalent for Routing Table in Router is Routes in React, also having empty Routing table dont do anything , have empty Routes component do not works! */
+    <Router>
+      <Routes>
+          <Route path="/" element={<Navigate replace to="/login"/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path='/register' element={<RegisterPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
