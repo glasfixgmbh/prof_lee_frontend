0557e1eec01d0a6d79aecfeac7d9fcfcb54a7e39
juliaklam <Julia.schulze1@gmx.net>
2025-09-22 11:29:46 +0200

    Routing und Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import ShopPage from "./pages/ShopPage"
+import ContactPage from "./pages/ContactPage"
+import DashbordPage from "./pages/DashbordPage";
+ 
+ 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+  <Routes>
+    <Route path="/"   element={<Navigate replace to="/login"/>}/>
+    <Route path="/login"   element={<LoginPage/>}/>
+    <Route path="/register"   element={<RegisterPage/>}/>
+    <Route path="/shop"   element={<ShopPage/>}/>
+    <Route path="/contact"   element={<ContactPage/>}/>
+    <Route path="/dashbord"   element={<DashbordPage/>}/>
+
+  </Routes>
+</Router>
   );
 }
 
---
+++ src/pages/WillkommenPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WillkommenPage() {
+  return (
+    <div>Willkommen bla bla</div>
+  )
+}
+
+export default WillkommenPage
